fa168d0894d10636e3077c8375cfeb979ef54783
Priyansh Kumar <priyanshkumar0077@gmail.com>
2026-04-30 13:44:03 +0530

    updated frontend work

--- src/main/java/BusinessAnalysis/system/config/CorsConfig.java
+++ src/main/java/BusinessAnalysis/system/config/CorsConfig.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.beans.factory.annotation.Value;
 @Configuration
+
 public class CorsConfig {
+    @Value("${app.frontend-url}")
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +19,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React app
+                        .allowedOrigins(
+                                frontendUrl,                 // ✅ production (Vercel)
+                                "http://localhost:5173"     // ✅ local dev
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
